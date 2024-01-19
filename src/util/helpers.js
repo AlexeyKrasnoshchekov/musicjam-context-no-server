7a4a70c3aaf1d6f5003973647e03be8cbced1d02
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2024-01-19 14:07:32 +0300

    fixes

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,6 +1,8 @@
-const clientId = "8ff9d45baf5b4833895018315692b108"; // Insert client ID here.
+// const clientId = "8ff9d45baf5b4833895018315692b108"; // Insert client ID here.
+const clientId = "8145b681e164401fa1e81b5d56ef601c"; // Insert client ID here.
 // const redirectUri = "http://musicjam-alex.surge.sh/callback"; 
-const redirectUri = "http://localhost:3000/callback"; 
+// const redirectUri = "http://localhost:3000/callback"; 
+const redirectUri = "https://musicjam2.vercel.app/callback"; 
 // const redirectUri = "https://alexeykrasnoshchekov.github.io/musicjam/callback";
 
 export const getAccessToken = () => {
